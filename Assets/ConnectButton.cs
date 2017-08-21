5951a6edf59f7e543276a214dd554a2d4521007b
Daisuke Hirata <daisukihirata@gmail.com>
2017-08-21 10:31:30 +0900

    Add connect button

---
+++ Assets/ConnectButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectButton : MonoBehaviour {
+	private int counter = 0;
+
+	void Start()
+	{
+	}
+
+	public void OnClick()
+	{
+		Debug.Log("Button Click: " + counter);
+		counter++;
+	}
+}
